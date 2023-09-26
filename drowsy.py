a84b107371a0847eb4a89233e6fb57730fd1f590
joshiansu <122740259+joshiansu@users.noreply.github.com>
2023-09-26 18:14:28 +0530

    drowsy code added

---
+++ drowsy.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, Response
+from scipy.spatial import distance
+from imutils import face_utils
+import imutils
+import dlib
+import cv2
+
+app = Flask(__name__)
+
+def eye_aspect_ratio(eye):
+	A = distance.euclidean(eye[1], eye[5])
+	B = distance.euclidean(eye[2], eye[4])
+	C = distance.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def detect_drowsiness():
+    thresh = 0.25
+    frame_check = 2
+    detect = dlib.get_frontal_face_detector()
+    predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+    cap = cv2.VideoCapture(0)
+    flag = 0
+
+    while True:
+        ret, frame = cap.read()
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        subjects = detect(gray, 0)
+        for subject in subjects:
+            shape = predict(gray, subject)
+            shape = face_utils.shape_to_np(shape)
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+            if ear < thresh:
+                flag += 1
+                if flag >= frame_check:
+                    alert_text = "****************ALERT!****************"
+                    cv2.putText(frame, alert_text, (10, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    cv2.putText(frame, alert_text, (10, 325),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        _, jpeg = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(detect_drowsiness(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
