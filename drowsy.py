0c508e34fe0557a4246f29d40db1ea312439c788
joshiansu <122740259+joshiansu@users.noreply.github.com>
2023-09-30 17:28:25 +0530

    flag reset added

--- drowsy.py
+++ drowsy.py
@@ -4,6 +4,7 @@ from imutils import face_utils
 import imutils
 import dlib
 import cv2
+import time
 
 app = Flask(__name__)
 
@@ -20,13 +21,14 @@ def index():
 
 def detect_drowsiness():
     thresh = 0.25
-    frame_check = 2
+    frame_check = 20
     detect = dlib.get_frontal_face_detector()
     predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
     cap = cv2.VideoCapture(0)
     flag = 0
+    alert_start_time = None
 
     while True:
         ret, frame = cap.read()
@@ -46,13 +48,28 @@ def detect_drowsiness():
             cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
             cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
             if ear < thresh:
+                # if alert_start_time is None:
+                #     alert_start_time = time.time()
+                # else:
+                #     alert_duration = time.time() - alert_start_time
+                #     if alert_duration >= 2.0:
+                #         flag = 0
+                #         alert_start_time = None
                 flag += 1
+                alert_text = "*************{} FRAMES****************".format(flag)
+                cv2.putText(frame, alert_text, (10, 220),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(frame, alert_text, (10, 325),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 if flag >= frame_check:
+                    print("ALERT SENT")
                     alert_text = "****************ALERT!****************"
                     cv2.putText(frame, alert_text, (10, 30),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                     cv2.putText(frame, alert_text, (10, 325),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            else:
+                flag = 0
         _, jpeg = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
